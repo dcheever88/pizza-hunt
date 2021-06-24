239b0eefb6ffc628c2212090e791115c9b2960a2
dcheever <cheeverdanny@gmail.com>
2021-06-24 14:47:14 -0500

    added comment and reply and pizza validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -48,10 +48,10 @@ const pizzaController = {
 
     // update pizza by id
     updatePizza({ params, body}, res) {
-        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true})
+        Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza foudn with this id'});
+                    res.status(404).json({ message: 'No pizza found with this id'});
                     return;
                 }
                 res.json(dbPizzaData);
--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,14 @@ const ReplySchema = new Schema(
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: 'No reply entered!',
+            trim: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: 'Tell us who you are!',
+            trim: true
         },
         createdAt: {
             type: Date,
@@ -29,10 +33,14 @@ const ReplySchema = new Schema(
 
 const CommentSchema = new Schema({
     writtenBy: {
-        type: String
+        type: String,
+        required: 'Tell us who you are!',
+        trim: true
     },
     commentBody: {
-        type: String
+        type: String,
+        required: 'No comment entered!',
+        trim: true
     },
     createdAt: {
         type: Date,
